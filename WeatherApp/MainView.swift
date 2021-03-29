9fa7b2b81648a2ebd55ba5dc0b26378bc0cf1578
Andrii Moisol <andrii.moisol@gmail.com>
2021-03-29 21:23:59 +0300

    Remove scene delegate

--- WeatherApp/MainView.swift
+++ WeatherApp/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  WeatherApp
 //
 //  Created by Andrii Moisol on 29.03.2021.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
